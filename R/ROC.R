f7666fe15cf563b1dc0d22cd87e9c21c73693706
Wush Wu <wush978@gmail.com>
2014-02-25 21:00:10 +0800

    add example

--- R/ROC.R
+++ R/ROC.R
@@ -4,6 +4,12 @@
 #'@param resize integer vector or \code{NULL}(default). If \code{is.null(resize)}, the complete ROC curve is returned.
 #'Otherwise, the number of returned points is down sampled to \code{resize}.
 #'@return \code{list(x, y)}. The x-coordinate and y-coordinate of the ROC curve. 
+#'@examples
+#'n <- 10^5
+#'pred.x <- rnorm(n)
+#'true.x <- runif(n) < 1 / (1 + exp(-pred.x))
+#'roc <- ROC(true.x, pred.x)
+#'auc <- AUC(roc$x, roc$y)
 #'@export
 ROC <- function(y.true, y.estimate, resize = NULL) {
   y.true <- as.integer(!y.true)
